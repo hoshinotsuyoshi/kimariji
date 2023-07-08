455cd98c123eca1e118944c8853ce85285ac369f
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2023-07-08 15:44:31 +0900

    初期実装

--- lib/kimariji.rb
+++ lib/kimariji.rb
@@ -1,8 +1,29 @@
 # frozen_string_literal: true
 
-require_relative "kimariji/version"
+require_relative 'kimariji/version'
 
 module Kimariji
-  class Error < StandardError; end
-  # Your code goes here...
+  # NOTE: 引数argはString
+  def self.candidate_下の句s(arg)
+    上の句と下の句のペア = [
+      ['すみのえの', 'ゆめのかよひちひとめよくらむ'],
+      ['ほととぎす', 'たたありあけのつきそのこれる'],
+      ['むらさめの', 'きりたちのほるあきのゆふくれ'],
+
+      ['うかりける', 'はけしかれとはいのらぬものを'],
+      ['うらみわび', 'こひにくちなむなこそをしけれ'],
+      ['つきみれば', 'わかみひとつのあきにはあらねと'],
+      ['つくばねの', 'こひそつもりてふちとなりぬる'],
+    ]
+
+    answer = []
+
+    上の句と下の句のペア.map do |上の句, 下の句|
+      if 上の句.start_with?(arg)
+        answer << 下の句
+      end
+    end
+
+    answer
+  end
 end
--- spec/kimariji_spec.rb
+++ spec/kimariji_spec.rb
@@ -1,11 +1,26 @@
 # frozen_string_literal: true
 
 RSpec.describe Kimariji do
-  it "has a version number" do
-    expect(Kimariji::VERSION).not_to be nil
+  describe '1文字目が決まり字(す など)' do
+    it '引数が"す"のとき 候補を1つ返す' do
+      expect(Kimariji.candidate_下の句s('す')).to eq(['ゆめのかよひちひとめよくらむ'])
+    end
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe '2文字目が決まり字(う,つ など)' do
+    it '引数が"う"のとき 候補を2つ返す' do
+      expect(Kimariji.candidate_下の句s('う')).to eq([
+        'はけしかれとはいのらぬものを',
+        'こひにくちなむなこそをしけれ',
+      ])
+    end
+
+    it '引数が"うか"のとき 候補を1つ返す' do
+      expect(Kimariji.candidate_下の句s('うか')).to eq(['はけしかれとはいのらぬものを'])
+    end
+
+    it '引数が"うら"のとき 候補を1つ返す' do
+      expect(Kimariji.candidate_下の句s('うら')).to eq(['こひにくちなむなこそをしけれ'])
+    end
   end
 end
