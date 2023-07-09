4d75e841779c76069d4d21ed0c1b958895968003
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2023-07-10 00:20:18 +0900

    引数が空文字のときの時の現状の挙動についてのテストを追加

--- spec/kimariji_spec.rb
+++ spec/kimariji_spec.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 RSpec.describe Kimariji do
+  describe '空文字' do
+    it '引数が空文字のとき 空でない配列を返す' do
+      expect(Kimariji.candidate_下の句s('')).not_to be_empty
+    end
+  end
+
   describe '1文字目が決まり字(す など)' do
     it '引数が"す"のとき 候補を1つ返す' do
       expect(Kimariji.candidate_下の句s('す')).to eq(['ゆめのかよひちひとめよくらむ'])
