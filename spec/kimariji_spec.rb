2222d820e5cfa31106b0bf6b85b7867efdb7976c
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2023-07-10 00:40:47 +0900

    「さ」に対応

--- spec/kimariji_spec.rb
+++ spec/kimariji_spec.rb
@@ -8,6 +8,10 @@ RSpec.describe Kimariji do
   end
 
   describe '1文字目が決まり字(す など)' do
+    it '引数が"さ"のとき 候補を1つ返す' do
+      expect(Kimariji.candidate_下の句s('さ')).to eq(['いつこもおなしあきのゆふくれ'])
+    end
+
     it '引数が"す"のとき 候補を1つ返す' do
       expect(Kimariji.candidate_下の句s('す')).to eq(['ゆめのかよひちひとめよくらむ'])
     end
